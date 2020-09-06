cfa018eaa0170d22d435e1ccc4dcbee1732b268f
ZhdanovP <pzhdanov@luxoft.com>
2020-09-06 22:09:51 +0300

    Initial commit

---
+++ src/string_calc.cpp
@@ -0,0 +1,20 @@
+#include "string_calc.hpp"
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+StringCalc::StringCalc()
+{
+}
+
+
+StringCalc::~StringCalc()
+{
+}
+
+
+int StringCalc::Add(string numbers)
+{
+	return 0;//Not Implemented yet
+}
---
+++ test/main_tests/test.cpp
@@ -0,0 +1,15 @@
+#include "gtest/gtest.h"
+#include "string_calc.hpp"
+#include <iostream>
+
+TEST(CalculatorTest, TestName) {
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
+}
+
+// TEST(CalculatorTest, SampleTest) {
+// 	StringCalc c;
+// 	int actual = c.Add("2,2");
+// 	ASSERT_EQ(actual, 4);
+// }
+
