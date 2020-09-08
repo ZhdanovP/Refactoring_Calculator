55a6092d7d6e581ec830690891a177440dcb17a0
LinuxUser404 <nikolay.pershyn@gmail.com>
2020-09-08 11:00:07 +0300

    Initial add implementation with tests

--- MykolaPershyn/src/string_calc.cpp
+++ MykolaPershyn/src/string_calc.cpp
@@ -1,6 +1,8 @@
 #include "string_calc.hpp"
 #include <vector>
 #include <algorithm>
+#include <sstream>
+#include <iostream>
 
 using namespace std;
 
@@ -16,5 +18,14 @@ StringCalc::~StringCalc()
 
 int StringCalc::Add(string numbers)
 {
+    if("" == numbers) return 0;
+    int a;
+    int b;
+    sscanf(numbers.c_str(), "%d,%d", &a, &b);
+//     cout << "a: " << a << "\n";
+//     cout << "b: " << b << "\n";
+    if(a < 0) return -1;
+    if(b < 0) return -1;
+    return a + b;
 	return 0;//Not Implemented yet
 }
--- MykolaPershyn/test/main_tests/test.cpp
+++ MykolaPershyn/test/main_tests/test.cpp
@@ -7,9 +7,44 @@ TEST(CalculatorTest, TestName) {
   EXPECT_TRUE(true);
 }
 
-// TEST(CalculatorTest, SampleTest) {
-// 	StringCalc c;
-// 	int actual = c.Add("2,2");
-// 	ASSERT_EQ(actual, 4);
-// }
+TEST(CalculatorTest, SampleTest) {
+  StringCalc c;
+  int actual = c.Add("2,2");
+  ASSERT_EQ(actual, 4);
+}
 
+TEST(CalculatorTest, EmptyString) {
+  StringCalc c;
+  int actual = c.Add("0");
+  ASSERT_EQ(actual, 0);
+}
+
+TEST(CalculatorTest, Zero) {
+  StringCalc c;
+  int actual = c.Add("0");
+  ASSERT_EQ(actual, 0);
+}
+
+TEST(CalculatorTest, SingleNumber) {
+  StringCalc c;
+  int actual = c.Add("1");
+  ASSERT_EQ(actual, 1);
+}
+
+TEST(CalculatorTest, SimpleSum) {
+  StringCalc c;
+  int actual = c.Add("1,2");
+  ASSERT_EQ(actual, 3);
+}
+
+TEST(CalculatorTest, WrongArgSign) {
+  StringCalc c;
+  int actual = c.Add("-1,-1");
+  ASSERT_EQ(actual, -1);
+}
+
+TEST(CalculatorTest, WrongArgLetters) {
+  StringCalc c;
+  int actual = c.Add("0abc1");
+  ASSERT_EQ(actual, -1);
+}
