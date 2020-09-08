a5948ca3ed274d0214f6aa698c62f22937ad70eb
LinuxUser404 <nikolay.pershyn@gmail.com>
2020-09-08 11:37:27 +0300

    Improved error handling

--- MykolaPershyn/src/string_calc.cpp
+++ MykolaPershyn/src/string_calc.cpp
@@ -15,17 +15,50 @@ StringCalc::~StringCalc()
 {
 }
 
+namespace {
+    string parseNum(string s, int &a, int &parsed) {
+        a = 0;
+        parsed = 0;
+        for(char c : s) {
+            if('0' < c && c < '9')
+            {
+                ++parsed;
+                a *= 10;
+                a += c - '0';
+            }
+            else
+                break;
+        }
+        return s.substr(parsed);
+    }
+};
 
 int StringCalc::Add(string numbers)
 {
+    // empty case
     if("" == numbers) return 0;
     int a;
     int b;
-    sscanf(numbers.c_str(), "%d,%d", &a, &b);
-//     cout << "a: " << a << "\n";
-//     cout << "b: " << b << "\n";
-    if(a < 0) return -1;
-    if(b < 0) return -1;
-    return a + b;
-	return 0;//Not Implemented yet
+    int parsed;
+    
+    // first number
+    string rest = ::parseNum(numbers, a, parsed);
+    if(0 == parsed) return -1; // expecting a number
+    if(numbers.size() == parsed) {
+        // the string contains just one number
+        return a;
+    }
+    
+    // coma
+    if(',' != rest[0]) return -1;
+    rest = rest.substr(1);
+    
+    // second number
+    string rest2 = ::parseNum(rest, b, parsed);
+    if(0 == parsed) return -1; // expecting a number
+    if(rest.size() == parsed) {
+        return a + b;
+    }
+
+    return -1;
 }
