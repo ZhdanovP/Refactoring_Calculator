8f0f1836290aa8f02fe6aeba0b26125c681b9206
Oleksandr Kunichik <OKunichik@luxoft.com>
2020-09-08 11:55:27 +0300

    split fixed, added simple 4 tests, one doesn't works (with a letter)

--- Alex.Kunichik/src/string_calc.cpp
+++ Alex.Kunichik/src/string_calc.cpp
@@ -21,19 +21,28 @@ StringCalc::~StringCalc()
 
 std::vector<std::string> split(std::string str)
 {
-    std::istringstream iss(str);
-    std::copy(std::istream_iterator<std::string>(iss),
-         std::istream_iterator<std::string>(),
-         std::ostream_iterator<std::string>(std::cout, "\n"));
+    std::istringstream ss(str);
+    std::string token;
+	std::vector<std::string> result;
+    
+    while(std::getline(ss, token, ',')) {
+        //std::cout << token << '\n';
+		result.push_back(token);
+    }
+
+    return result;
 }
 
 int StringCalc::Add(string numbers)
 {
 	auto operands = split(numbers);
+	//std::cout << "operands done";
 	int result {0};
 	for(auto num : operands)
 	{
-		result += atoi(num.data());
+		result += std::atoi(num.c_str());
+		std::cout << result;
 	}
 	return result;
+	//std::cout << result;
 }
--- Alex.Kunichik/test/main_tests/test.cpp
+++ Alex.Kunichik/test/main_tests/test.cpp
@@ -13,3 +13,31 @@ TEST(CalculatorTest, TestName) {
 	ASSERT_EQ(actual, 4);
 }
 
+
+
+ TEST(CalculatorTest, TwoOperands) {
+ 	StringCalc c;
+	int actual = c.Add("18, 17");
+	ASSERT_EQ(actual, 35);
+}
+
+
+ TEST(CalculatorTest, NoneOperands) {
+ 	StringCalc c;
+	int actual = c.Add("");
+	ASSERT_EQ(actual, 0);
+}
+
+
+ TEST(CalculatorTest, OneOperand) {
+ 	StringCalc c;
+	int actual = c.Add("9");
+	ASSERT_EQ(actual, 9);
+}
+
+
+ TEST(CalculatorTest, OneLetterAndNumber) {
+ 	StringCalc c;
+	int actual = c.Add("18, d");
+	ASSERT_EQ(actual, 18);
+}
