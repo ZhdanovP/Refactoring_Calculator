a426fb5f4868a06244eab0f80dd3ccd95b6c5ace
mamozavr <andremamonto@gmail.ru>
2020-09-09 01:49:59 +0300

    Step 1

--- src/string_calc.cpp
+++ src/string_calc.cpp
@@ -1,6 +1,8 @@
 #include "string_calc.hpp"
 #include <vector>
 #include <algorithm>
+#include <regex>
+#include <iostream>
 
 using namespace std;
 
@@ -16,5 +18,31 @@ StringCalc::~StringCalc()
 
 int StringCalc::Add(string numbers)
 {
-	return 0;//Not Implemented yet
+  
+    const std::regex rgx("[0-9]|,");
+
+    if(std::regex_replace(numbers,rgx,"").length()){
+         return -1;
+    }
+
+    unsigned int result = 0;
+    size_t endPos = 0;
+    size_t curPos = 0;
+    char separator =','; 
+
+      while(curPos<numbers.length()){
+   
+           endPos = numbers.find(separator,curPos);
+           
+           if(endPos==std::string::npos){
+               endPos = numbers.length();    
+           }                  
+           
+                  
+           result += stoi(numbers.substr(curPos,endPos - curPos));
+
+           curPos = endPos+1; 
+      }             
+    
+    return result;
 }
--- test/main_tests/test.cpp
+++ test/main_tests/test.cpp
@@ -2,10 +2,28 @@
 #include "string_calc.hpp"
 #include <iostream>
 
-TEST(CalculatorTest, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
-}
+class CalculatorTest : public ::testing::Test {
+  
+public:
+    StringCalc m_Calculator;
+};
+
+TEST_F(CalculatorTest, TestEmptyString) {
+  EXPECT_EQ(0, m_Calculator.Add(""));
+ }
+
+TEST_F(CalculatorTest, TestSum) {
+  EXPECT_EQ(15, m_Calculator.Add("12,3"));
+ }
+
+TEST_F(CalculatorTest, TestWrongArguments) {
+  EXPECT_EQ(-1, m_Calculator.Add("2,a3"));
+ }
+
+TEST_F(CalculatorTest, TestCommaAsLast) {
+  EXPECT_EQ(23, m_Calculator.Add("19,4,"));
+ }
+
 
 // TEST(CalculatorTest, SampleTest) {
 // 	StringCalc c;
