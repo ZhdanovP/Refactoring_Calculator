cbaec3f7a01a63b398595e4e49d1914adc961137
Anton Chernousov <anthonyztl@mail.ru>
2020-09-09 09:55:00 +0300

    - step 2

--- chernousov/test/main_tests/test.cpp
+++ chernousov/test/main_tests/test.cpp
@@ -48,28 +48,58 @@ StringCalc c;
 int actual = c.Add("a,");
 ASSERT_EQ(actual, -1);
 }
+
 TEST(CalculatorTest, SampleTest_8) {
 StringCalc c;
 int actual = c.Add(",A");
 ASSERT_EQ(actual, -1);
 }
+
 TEST(CalculatorTest, SampleTest_9) {
 StringCalc c;
 int actual = c.Add("2,F");
 ASSERT_EQ(actual, -1);
 }
+
 TEST(CalculatorTest, SampleTest_10) {
 StringCalc c;
 int actual = c.Add("3asd,");
 ASSERT_EQ(actual, -1);
 }
+
 TEST(CalculatorTest, SampleTest_11) {
 StringCalc c;
 int actual = c.Add("2,45ass");
 ASSERT_EQ(actual, -1);
 }
+
 TEST(CalculatorTest, SampleTest_12) {
 StringCalc c;
 int actual = c.Add("g6j9");
 ASSERT_EQ(actual, -1);
+}
+
+TEST(CalculatorTest, SampleTest_13) {
+StringCalc c;
+int actual = c.Add(",6,4145,2314");
+ASSERT_EQ(actual, 6465);
+}
+
+TEST(CalculatorTest, SampleTest_14) {
+StringCalc c;
+int actual = c.Add("3214,243,3421");
+ASSERT_EQ(actual, 6878);
+}
+
+
+TEST(CalculatorTest, SampleTest_15) {
+StringCalc c;
+int actual = c.Add(",,,,,,,");
+ASSERT_EQ(actual, 0);
+}
+
+TEST(CalculatorTest, SampleTest_16) {
+StringCalc c;
+int actual = c.Add("3214,243,");
+ASSERT_EQ(actual, 3457);
 }
