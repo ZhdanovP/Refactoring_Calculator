39871f92049f0c58105ad91a02c9ffbebb969811
mamozavr <andremamonto@gmail.ru>
2020-09-09 11:39:30 +0300

    refac

--- include/string_calc.hpp
+++ include/string_calc.hpp
@@ -2,6 +2,7 @@
 #define STRING_CALC_HPP
 
 #include <string>
+#include <set>
 
 class StringCalc
 {
@@ -11,6 +12,19 @@ public:
 	~StringCalc();
 	
 	int Add(std::string numbers);
+         
+private:
+        void dropState();
+        void getEndOfTheNextNumber();  
+        void parseSeparators(); 
+        void checkWrongArguments();
+        void setNumbers(std::string numbers); 
+
+        std::set<std::string> separators;
+        std::string m_Numbers; 
+        size_t m_EndPos;
+        size_t m_CurPos;
+        size_t m_Offs;
 };
 
 #endif
