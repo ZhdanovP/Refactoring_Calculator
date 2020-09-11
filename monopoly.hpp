827c2fd95748ddd22fabf6887f797947e6e1f79e
LinuxUser404 <nikolay.pershyn@gmail.com>
2020-09-11 09:56:07 +0300

    Fix format

--- monopoly.hpp
+++ monopoly.hpp
@@ -10,52 +10,52 @@
 class Monopoly
 {
 public:
-    struct Player {
-        std::string name;
-        int money;
-    };
-
-	enum Type
-	{
-		AUTO,
-		FOOD,
-		TRAVEL,
-		CLOTHER,
-		PRISON,
-		BANK
-	};
-    
-    constexpr std::optional<int> price(Type t) {
-        switch(t) {
-            case AUTO: return 500;
-            case FOOD: return 250;
-            case TRAVEL: return 700;
-            case CLOTHER: return 100;
-//             case PRISON: return 1100;
-//             case BANK: return 700;
-            default: return {};
-        }
+  struct Player {
+    std::string name;
+    int money;
+  };
+  
+  enum Type
+  {
+    AUTO,
+    FOOD,
+    TRAVEL,
+    CLOTHER,
+    PRISON,
+    BANK
+  };
+  
+  constexpr std::optional<int> price(Type t) {
+    switch(t) {
+      case AUTO: return 500;
+      case FOOD: return 250;
+      case TRAVEL: return 700;
+      case CLOTHER: return 100;
+      // case PRISON: return 1100;
+      // case BANK: return 700;
+      default: return {};
     }
-
-    struct field_t {
-        std::string name;
-        Type type;
-        int owner;
-    };
-
-    [[deprecated("Use Monopoly(std::initializer_list<const char*>& names); instead.")]]
-    Monopoly(std::string names[10],int);
-    Monopoly(std::initializer_list<const char*>& names);
-
-    std::list<Player> *	GetPlayersList();
-	std::list<field_t> * GetFieldsList();
-	std::list<field_t> Fields;
-	std::list<Player> Players;
-	Player  GetPlayerInfo(int);	 
-	bool Buy(int p, field_t);
-	field_t GetFieldByName(std::string) const;	
-	bool Renta(int p, field_t c);
-	
+  }
+  
+  struct field_t {
+    std::string name;
+    Type type;
+    int owner;
+  };
+  
+  [[deprecated("Use Monopoly(std::initializer_list<const char*>& names); instead.")]]
+  Monopoly(std::string names[10],int);
+  Monopoly(std::initializer_list<const char*>& names);
+  
+  std::list<Player> *	GetPlayersList();
+  std::list<field_t> * GetFieldsList();
+  std::list<field_t> Fields;
+  std::list<Player> Players;
+  Player  GetPlayerInfo(int);	 
+  bool Buy(int p, field_t);
+  field_t GetFieldByName(std::string) const;	
+  bool Renta(int p, field_t c);
+  
 };
 
 #endif
--- test.cpp
+++ test.cpp
@@ -5,68 +5,68 @@
 using namespace ::std;
 
 TEST(LAB2, GetPlayersListReturnCorrectList) {
-    auto players = { "Peter","Ekaterina","Alexander" };
-   
-    Monopoly monopoly(players);
-
-    list<Monopoly::Player>* x = monopoly.GetPlayersList();
-    auto p = players.begin();
-    for (auto c : *x) {
-        ASSERT_STREQ(c.name.c_str(), *(p++));
-        ASSERT_EQ(c.money, 6000);
-    }
+  auto players = { "Peter","Ekaterina","Alexander" };
+  
+  Monopoly monopoly(players);
+  
+  list<Monopoly::Player>* x = monopoly.GetPlayersList();
+  auto p = players.begin();
+  for (auto c : *x) {
+    ASSERT_STREQ(c.name.c_str(), *(p++));
+    ASSERT_EQ(c.money, 6000);
+  }
 }
 TEST(LAB2, GetFieldsListReturnCorrectList) {
-   Monopoly::field_t expectedCompanies[]{
-       {"Ford",Monopoly::AUTO ,0},
-       {"MCDonald",Monopoly::FOOD,0},
-       {"Lamoda",Monopoly::CLOTHER,0},
-       {"Air Baltic",Monopoly::TRAVEL,0},
-       {"Nordavia",Monopoly::TRAVEL,0},
-       {"Prison",Monopoly::PRISON,0},
-       {"MCDonald",Monopoly::FOOD,0},
-       {"TESLA",Monopoly::AUTO,0}
-   };
-   auto players = { "Peter","Ekaterina","Alexander" };
-
-   Monopoly monopoly(players);
-   auto actualCompanies = monopoly.GetFieldsList();
-   int i = 0;
-   for (auto x : *actualCompanies)
-   {
-       ASSERT_EQ(x, expectedCompanies[i++]);
-   }
-   ASSERT_TRUE(i);   
+  Monopoly::field_t expectedCompanies[]{
+    {"Ford",Monopoly::AUTO ,0},
+    {"MCDonald",Monopoly::FOOD,0},
+    {"Lamoda",Monopoly::CLOTHER,0},
+    {"Air Baltic",Monopoly::TRAVEL,0},
+    {"Nordavia",Monopoly::TRAVEL,0},
+    {"Prison",Monopoly::PRISON,0},
+    {"MCDonald",Monopoly::FOOD,0},
+    {"TESLA",Monopoly::AUTO,0}
+  };
+  auto players = { "Peter","Ekaterina","Alexander" };
+  
+  Monopoly monopoly(players);
+  auto actualCompanies = monopoly.GetFieldsList();
+  int i = 0;
+  for (auto x : *actualCompanies)
+  {
+    ASSERT_EQ(x, expectedCompanies[i++]);
+  }
+  ASSERT_TRUE(i);   
 }
 
 TEST(LAB2, PlayerBuyNoOwnedCompanies)
 {
-    auto players = { "Peter","Ekaterina","Alexander" };
-
-    Monopoly monopoly(players);
-    auto x = monopoly.GetFieldByName("Ford");
-    monopoly.Buy(1, x);
-
-    auto player = monopoly.GetPlayerInfo(1);
-    ASSERT_EQ(player.money, 5500);
-    x = monopoly.GetFieldByName("Ford");
-    ASSERT_TRUE(x.owner != 0);
+  auto players = { "Peter","Ekaterina","Alexander" };
+  
+  Monopoly monopoly(players);
+  auto x = monopoly.GetFieldByName("Ford");
+  monopoly.Buy(1, x);
+  
+  auto player = monopoly.GetPlayerInfo(1);
+  ASSERT_EQ(player.money, 5500);
+  x = monopoly.GetFieldByName("Ford");
+  ASSERT_TRUE(x.owner != 0);
 }
 
 TEST(LAB2, RentaShouldBeCorrectTransferMoney)
 {
-    auto players = { "Peter","Ekaterina","Alexander" };
-    Monopoly monopoly(players);
-    auto x = monopoly.GetFieldByName("Ford");
-    monopoly.Buy(1, x);
-
-    x = monopoly.GetFieldByName("Ford");
-    monopoly.Renta(2, x);
-    auto player1 = monopoly.GetPlayerInfo(1);
-    ASSERT_EQ(player1.money, 5750);
-
-    auto player2 = monopoly.GetPlayerInfo(2);
-    ASSERT_EQ(player2.money, 5750);    
+  auto players = { "Peter","Ekaterina","Alexander" };
+  Monopoly monopoly(players);
+  auto x = monopoly.GetFieldByName("Ford");
+  monopoly.Buy(1, x);
+  
+  x = monopoly.GetFieldByName("Ford");
+  monopoly.Renta(2, x);
+  auto player1 = monopoly.GetPlayerInfo(1);
+  ASSERT_EQ(player1.money, 5750);
+  
+  auto player2 = monopoly.GetPlayerInfo(2);
+  ASSERT_EQ(player2.money, 5750);    
 }
 
 
