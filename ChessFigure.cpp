3ec8f1f11ba1cdb14c1a6ac91aab778b69eab8da
Oleksii Kuchuk <OKuchuk@luxoft.com>
2020-09-14 18:29:08 +0300

    Use dynamic polymorphism

--- ChessFigure.cpp
+++ ChessFigure.cpp
@@ -1,93 +1,78 @@
 #include "ChessFigure.h"
-#include <string>
 #include <cmath>
+#include <string>
 #include <utility>
 
 using namespace std;
 
-bool SameDigit(const Coord& c1, const Coord& c2) {
-	return c1.digit_ == c2.digit_;
+bool SameDigit(const Coord &c1, const Coord &c2) {
+  return c1.digit_ == c2.digit_;
 }
 
-bool SameLetter(const Coord& c1, const Coord& c2) {
-	return c1.letter_ == c2.letter_;
+bool SameLetter(const Coord &c1, const Coord &c2) {
+  return c1.letter_ == c2.letter_;
 }
 
-bool SameLine(const Coord& c1, const Coord& c2) {
-	return SameDigit(c1, c2) || SameLetter(c1, c2);
+bool SameLine(const Coord &c1, const Coord &c2) {
+  return SameDigit(c1, c2) || SameLetter(c1, c2);
 }
 
-bool SameDiag(const Coord& c1, const Coord& c2) {
-	return abs(c1.letter_ - c2.letter_) == abs(c1.digit_ - c2.digit_);
+bool SameDiag(const Coord &c1, const Coord &c2) {
+  return abs(c1.letter_ - c2.letter_) == abs(c1.digit_ - c2.digit_);
 }
 
-int AbsMaxDistance(const Coord& c1, const Coord& c2) {
-	return max(abs(c1.DigitDistance(c2)), abs(c1.LetterDistance(c2)));
+int AbsMaxDistance(const Coord &c1, const Coord &c2) {
+  return max(abs(c1.DigitDistance(c2)), abs(c1.LetterDistance(c2)));
 }
 
-int AbsDistanceSquared(const Coord& c1, const Coord& c2) {
-	auto dx = c1.DigitDistance(c2);
-	auto dy = c1.LetterDistance(c2);
-	return dx * dx + dy * dy;
+int AbsDistanceSquared(const Coord &c1, const Coord &c2) {
+  auto dx = c1.DigitDistance(c2);
+  auto dy = c1.LetterDistance(c2);
+  return dx * dx + dy * dy;
 }
 
-ChessFigure::ChessFigure(ChessFigure::FigureType type, Coord coord) : type(type),
-currentCoord(std::move(coord))
-{
+bool Pawn::Move(Coord nextCoord) const {
+  if (!SameLetter(currentCoord, nextCoord) ||
+      currentCoord.DigitDistance(nextCoord) <= 0 ||
+      (currentCoord.DigitDistance(nextCoord) != 1 &&
+       (currentCoord.Digit() != '2' || nextCoord.Digit() != '4')))
+    return false;
+  else
+    return true;
 }
 
-
-ChessFigure::~ChessFigure()
-{
+bool Rook::Move(Coord nextCoord) const {
+  if (!SameLine(currentCoord, nextCoord))
+    return false;
+  else
+    return true;
 }
 
-bool ChessFigure::Move(Coord nextCoord)
-{
-	if (type == PAWN)
-	{
-			if (!SameLetter(currentCoord, nextCoord) || currentCoord.DigitDistance(nextCoord) <= 0 || (currentCoord.DigitDistance(nextCoord) != 1 && (currentCoord.Digit() != '2' || nextCoord.Digit() != '4')))
-				return false;
-			else
-				return true;
-	}
-	
-	else if (type == ROOK)
-	{
-			if (!SameLine(currentCoord, nextCoord))
-				return false;
-			else
-				return true;
-	}
-	else if (type == KNIGHT)
-	{
-		  if (SameLine(currentCoord, nextCoord) || AbsDistanceSquared(currentCoord, nextCoord) != 5)
-			  return false;
-			else
-			return true;
-	}
-	
-	else if (type == BISHOP)
-	{
-			if (!SameDiag(currentCoord, nextCoord))
-				return false;
-			else
-				return true;
-	}
-	
-	else if (type == KING)
-	{
-			if (AbsMaxDistance(currentCoord, nextCoord) != 1)
-				return false;
-			else
-				return true;
-	}
-	else if (type == QUEEN)
-	{
-			if (!SameLine(currentCoord, nextCoord) && !SameDiag(currentCoord, nextCoord))
-				return false;
-			else
-				return true;
-	}
-	else
-		return false;
+bool Knight::Move(Coord nextCoord) const {
+  if (SameLine(currentCoord, nextCoord) ||
+      AbsDistanceSquared(currentCoord, nextCoord) != 5)
+    return false;
+  else
+    return true;
+}
+
+bool Bishop::Move(Coord nextCoord) const {
+  if (!SameDiag(currentCoord, nextCoord))
+    return false;
+  else
+    return true;
+}
+
+bool King::Move(Coord nextCoord) const {
+  if (AbsMaxDistance(currentCoord, nextCoord) != 1)
+    return false;
+  else
+    return true;
+}
+
+bool Queen::Move(Coord nextCoord) const {
+  if (!SameLine(currentCoord, nextCoord) && !SameDiag(currentCoord, nextCoord))
+    return false;
+  else
+    return true;
 }
--- ChessFigure.h
+++ ChessFigure.h
@@ -2,59 +2,93 @@
 #define CHESS_FIGURE_HPP
 
 #include <stdexcept>
+#include <utility>
 
 struct Coord {
-	Coord(char letter, char digit) : letter_{letter}, digit_{digit} {
-		if (letter_ < 'A' || letter_ > 'H')
-			throw std::invalid_argument{"Letter part of coordinate must be in A-H range"};
-		if (digit_ < '1' || digit_ > '8')
-			throw std::invalid_argument{"Digit part of coordinate must be in 1-8 range"};
-	}
+  Coord(char letter, char digit) : letter_{letter}, digit_{digit} {
+    if (letter_ < 'A' || letter_ > 'H')
+      throw std::invalid_argument{
+          "Letter part of coordinate must be in A-H range"};
+    if (digit_ < '1' || digit_ > '8')
+      throw std::invalid_argument{
+          "Digit part of coordinate must be in 1-8 range"};
+  }
 
-	Coord(const char coords[2]) : Coord(coords[0], coords[1]) {}
+  Coord(const char coords[2]) : Coord(coords[0], coords[1]) {}
 
-	int LetterDistance(const Coord& other) const {
-		return other.letter_ - letter_;
-	}
+  int LetterDistance(const Coord &other) const {
+    return other.letter_ - letter_;
+  }
 
-	int DigitDistance(const Coord& other) const {
-		return other.digit_ - digit_;
-	}
+  int DigitDistance(const Coord &other) const { return other.digit_ - digit_; }
 
-	char Digit() { return digit_; }
-	char Letter() { return letter_; }
+  char Digit() const { return digit_; }
+  char Letter() const { return letter_; }
 
-	friend bool SameLetter(const Coord& c1, const Coord& c2);
-	friend bool SameDigit(const Coord& c1, const Coord& c2);
-	friend bool SameDiag(const Coord& c1, const Coord& c2);
+  friend bool SameLetter(const Coord &c1, const Coord &c2);
+  friend bool SameDigit(const Coord &c1, const Coord &c2);
+  friend bool SameDiag(const Coord &c1, const Coord &c2);
 
 private:
-	char letter_;
-	char digit_;
+  char letter_;
+  char digit_;
 };
 
-
-
-
-class ChessFigure
-{
-	
+class ChessFigure {
 public:
-	enum FigureType
-	{
-		ROOK,
-		KNIGHT,
-		BISHOP,
-		PAWN,
-		KING,
-		QUEEN
-	};
-	ChessFigure(FigureType type, Coord coord);
-	bool Move(Coord nextCoord);
-	virtual ~ChessFigure();
-private:
-	FigureType type;
-	Coord currentCoord;
+  virtual bool Move(Coord nextCoord) const = 0;
+  virtual ~ChessFigure() {}
+};
+
+class RealChessFigure {
+public:
+  virtual ~RealChessFigure() {}
+  RealChessFigure(Coord coord) : currentCoord(std::move(coord)) {}
+
+protected:
+  Coord currentCoord;
+};
+
+class Pawn : public ChessFigure, protected RealChessFigure {
+public:
+  Pawn(Coord coord) : RealChessFigure(coord) {}
+
+  bool Move(Coord nextCoord) const override;
+};
+
+class Bishop : public ChessFigure, protected RealChessFigure {
+public:
+  Bishop(Coord coord) : RealChessFigure(coord) {}
+
+  bool Move(Coord nextCoord) const override;
+};
+
+class Knight : public ChessFigure, protected RealChessFigure {
+public:
+  Knight(Coord coord) : RealChessFigure(coord) {}
+
+  bool Move(Coord nextCoord) const override;
+};
+
+class Rook : public ChessFigure, protected RealChessFigure {
+public:
+  Rook(Coord coord) : RealChessFigure(coord) {}
+
+  bool Move(Coord nextCoord) const override;
+};
+
+class King : public ChessFigure, protected RealChessFigure {
+public:
+  King(Coord coord) : RealChessFigure(coord) {}
+
+  bool Move(Coord nextCoord) const override;
+};
+
+class Queen : public ChessFigure, protected RealChessFigure {
+public:
+  Queen(Coord coord) : RealChessFigure(coord) {}
+
+  bool Move(Coord nextCoord) const override;
 };
 
 #endif
