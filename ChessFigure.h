09e4ca2d22066a48bb17d7f1e9fd11dfc9a5ec0f
Oleksii Kuchuk <OKuchuk@luxoft.com>
2020-09-14 18:58:38 +0300

    Use template

--- ChessFigure.h
+++ ChessFigure.h
@@ -1,94 +1,58 @@
 #ifndef CHESS_FIGURE_HPP
 #define CHESS_FIGURE_HPP
 
-#include <stdexcept>
+#include "Coord.h"
+
 #include <utility>
 
-struct Coord {
-  Coord(char letter, char digit) : letter_{letter}, digit_{digit} {
-    if (letter_ < 'A' || letter_ > 'H')
-      throw std::invalid_argument{
-          "Letter part of coordinate must be in A-H range"};
-    if (digit_ < '1' || digit_ > '8')
-      throw std::invalid_argument{
-          "Digit part of coordinate must be in 1-8 range"};
-  }
-
-  Coord(const char coords[2]) : Coord(coords[0], coords[1]) {}
-
-  int LetterDistance(const Coord &other) const {
-    return other.letter_ - letter_;
-  }
-
-  int DigitDistance(const Coord &other) const { return other.digit_ - digit_; }
-
-  char Digit() const { return digit_; }
-  char Letter() const { return letter_; }
-
-  friend bool SameLetter(const Coord &c1, const Coord &c2);
-  friend bool SameDigit(const Coord &c1, const Coord &c2);
-  friend bool SameDiag(const Coord &c1, const Coord &c2);
+template <typename Tag> class ChessFigure {
+public:
+  ChessFigure(Coord coord) : currentCoord(std::move(coord)) {}
+  bool Move(Coord nextCoord) const;
 
 private:
-  char letter_;
-  char digit_;
-};
-
-class ChessFigure {
-public:
-  virtual bool Move(Coord nextCoord) const = 0;
-  virtual ~ChessFigure() {}
-};
-
-class RealChessFigure {
-public:
-  virtual ~RealChessFigure() {}
-  RealChessFigure(Coord coord) : currentCoord(std::move(coord)) {}
-
-protected:
   Coord currentCoord;
 };
 
-class Pawn : public ChessFigure, protected RealChessFigure {
-public:
-  Pawn(Coord coord) : RealChessFigure(coord) {}
+struct PawnTag {};
+struct BishopTag {};
+struct KnightTag {};
+struct RookTag {};
+struct KingTag {};
+struct QueenTag {};
 
-  bool Move(Coord nextCoord) const override;
-};
+using Pawn = ChessFigure<PawnTag>;
+using Bishop = ChessFigure<BishopTag>;
+using Knight = ChessFigure<KnightTag>;
+using Rook = ChessFigure<RookTag>;
+using King = ChessFigure<KingTag>;
+using Queen = ChessFigure<QueenTag>;
 
-class Bishop : public ChessFigure, protected RealChessFigure {
-public:
-  Bishop(Coord coord) : RealChessFigure(coord) {}
+template <> bool Pawn::Move(Coord nextCoord) const {
+  bool movesByOne = currentCoord.DigitDistance(nextCoord) == 1;
+  bool firstMoveByTwo = currentCoord.Digit() == '2' && nextCoord.Digit() == '4';
+  return SameLetter(currentCoord, nextCoord) && (movesByOne || firstMoveByTwo);
+}
 
-  bool Move(Coord nextCoord) const override;
-};
+template <> bool Bishop::Move(Coord nextCoord) const {
+  return SameDiag(currentCoord, nextCoord);
+}
 
-class Knight : public ChessFigure, protected RealChessFigure {
-public:
-  Knight(Coord coord) : RealChessFigure(coord) {}
+template <> bool Knight::Move(Coord nextCoord) const {
+  return !SameLine(currentCoord, nextCoord) &&
+         AbsDistanceSquared(currentCoord, nextCoord) == 5;
+}
 
-  bool Move(Coord nextCoord) const override;
-};
+template <> bool Rook::Move(Coord nextCoord) const {
+  return SameLine(currentCoord, nextCoord);
+}
 
-class Rook : public ChessFigure, protected RealChessFigure {
-public:
-  Rook(Coord coord) : RealChessFigure(coord) {}
+template <> bool King::Move(Coord nextCoord) const {
+  return AbsMaxDistance(currentCoord, nextCoord) == 1;
+}
 
-  bool Move(Coord nextCoord) const override;
-};
-
-class King : public ChessFigure, protected RealChessFigure {
-public:
-  King(Coord coord) : RealChessFigure(coord) {}
-
-  bool Move(Coord nextCoord) const override;
-};
-
-class Queen : public ChessFigure, protected RealChessFigure {
-public:
-  Queen(Coord coord) : RealChessFigure(coord) {}
-
-  bool Move(Coord nextCoord) const override;
-};
+template <> bool Queen::Move(Coord nextCoord) const {
+  return SameLine(currentCoord, nextCoord) || SameDiag(currentCoord, nextCoord);
+}
 
 #endif
--- test.cpp
+++ test.cpp
@@ -1,98 +1,82 @@
 #include "ChessFigure.h"
 #include <gtest/gtest.h>
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(LAB2, IncorrectCoord) {
+  auto *figure = new Pawn("E2");
+  EXPECT_THROW(figure->Move("i9"), std::invalid_argument);
 }
 
-TEST(LAB2, IncorrectCoord)
-{
-	ChessFigure *figure = new Pawn("E2");
-	EXPECT_THROW(figure->Move("i9"), std::invalid_argument);
+TEST(LAB2, RookShouldBeCorrectMove) {
+  auto *figure = new Rook("E2");
+  EXPECT_TRUE(figure->Move("C2"));
 }
 
-TEST(LAB2, RookShouldBeCorrectMove)
-{
-	ChessFigure *figure = new Rook("E2");
-	EXPECT_TRUE(figure->Move("C2"));
+TEST(LAB2, RookShouldBeIncorrectMove) {
+  auto *figure = new Rook("E2");
+  EXPECT_FALSE(figure->Move("C5"));
 }
 
-TEST(LAB2, RookShouldBeIncorrectMove)
-{
-	ChessFigure *figure = new Rook("E2");
-	EXPECT_FALSE(figure->Move("C5"));
-}
-TEST(LAB2, KnightShouldBeCorrectMove)
-{
-	ChessFigure *figure = new Knight("B1");
-	EXPECT_TRUE(figure->Move("C3"));
-}
-TEST(LAB2, KnightShouldBeIncorrectMove)
-{
-	ChessFigure *figure = new Knight("B1");
-	EXPECT_FALSE(figure->Move("C5"));
+TEST(LAB2, KnightShouldBeCorrectMove) {
+  auto *figure = new Knight("B1");
+  EXPECT_TRUE(figure->Move("C3"));
 }
 
-TEST(LAB2, BishopShouldBeCorrectMove)
-{
-	ChessFigure *figure = new Bishop("C1");
-	EXPECT_TRUE(figure->Move("E3"));
+TEST(LAB2, KnightShouldBeIncorrectMove) {
+  auto *figure = new Knight("B1");
+  EXPECT_FALSE(figure->Move("C5"));
 }
 
-TEST(LAB2, BishopShouldBeIncorrectMove)
-{
-	ChessFigure *figure = new Bishop("C1");
-	EXPECT_FALSE(figure->Move("C3"));
-}
-TEST(LAB2, PawnShouldBeCorrectMove0)
-{
-	ChessFigure* figure = new Pawn("E2");
-	EXPECT_TRUE(figure->Move("E3"));
+TEST(LAB2, BishopShouldBeCorrectMove) {
+  auto *figure = new Bishop("C1");
+  EXPECT_TRUE(figure->Move("E3"));
 }
 
-TEST(LAB2, PawnShouldBeCorrectMove1)
-{
-	ChessFigure *figure = new Pawn("E2");
-	EXPECT_TRUE(figure->Move("E4"));
+TEST(LAB2, BishopShouldBeIncorrectMove) {
+  auto *figure = new Bishop("C1");
+  EXPECT_FALSE(figure->Move("C3"));
 }
 
-TEST(LAB2, PawnShouldBeCorrectMove2)
-{
-	ChessFigure *figure = new Pawn("E4");
-	EXPECT_TRUE(figure->Move("E5"));
+TEST(LAB2, PawnShouldBeCorrectMove0) {
+  auto *figure = new Pawn("E2");
+  EXPECT_TRUE(figure->Move("E3"));
 }
 
-TEST(LAB2, PawnShouldBeIncorrectMove)
-{
-	ChessFigure *figure = new Pawn("E2");
-	EXPECT_FALSE(figure->Move("C5"));
-}
-TEST(LAB2, KingShouldBeCorrectMove)
-{
-	ChessFigure* figure = new King("E1");
-	EXPECT_TRUE(figure->Move("E2"));
-}
-TEST(LAB2, KingShouldBeIncorrectMove)
-{
-	ChessFigure* figure = new King("E1");
-	EXPECT_FALSE(figure->Move("E8"));
+TEST(LAB2, PawnShouldBeCorrectMove1) {
+  auto *figure = new Pawn("E2");
+  EXPECT_TRUE(figure->Move("E4"));
 }
 
-TEST(LAB2, QueenShouldBeCorrectMoveVertical)
-{
-	ChessFigure* figure = new Queen("D1");
-	EXPECT_TRUE(figure->Move("D8"));
+TEST(LAB2, PawnShouldBeCorrectMove2) {
+  auto *figure = new Pawn("E4");
+  EXPECT_TRUE(figure->Move("E5"));
 }
 
-TEST(LAB2, QueenShouldBeCorrectMoveDiagonal)
-{
-	ChessFigure* figure = new Queen("D1");
-	EXPECT_TRUE(figure->Move("H5"));
+TEST(LAB2, PawnShouldBeIncorrectMove) {
+  auto *figure = new Pawn("E2");
+  EXPECT_FALSE(figure->Move("C5"));
 }
 
-TEST(LAB2, QueenShouldBeIncorrectMove)
-{
-	ChessFigure* figure = new Queen("D1");
-	EXPECT_FALSE(figure->Move("E3"));
+TEST(LAB2, KingShouldBeCorrectMove) {
+  auto *figure = new King("E1");
+  EXPECT_TRUE(figure->Move("E2"));
+}
+
+TEST(LAB2, KingShouldBeIncorrectMove) {
+  auto *figure = new King("E1");
+  EXPECT_FALSE(figure->Move("E8"));
+}
+
+TEST(LAB2, QueenShouldBeCorrectMoveVertical) {
+  auto *figure = new Queen("D1");
+  EXPECT_TRUE(figure->Move("D8"));
+}
+
+TEST(LAB2, QueenShouldBeCorrectMoveDiagonal) {
+  auto *figure = new Queen("D1");
+  EXPECT_TRUE(figure->Move("H5"));
+}
+
+TEST(LAB2, QueenShouldBeIncorrectMove) {
+  auto *figure = new Queen("D1");
+  EXPECT_FALSE(figure->Move("E3"));
 }
