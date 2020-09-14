4e2457f6ae2c097fc4bfbd3f78219088ebb7cb99
a-kuchko <akuchko@luxoft.com>
2020-09-15 01:12:24 +0300

    Added Coordinate struct instead of array indexes, defined border constants

--- include/ChessFigure.hpp
+++ include/ChessFigure.hpp
@@ -15,14 +15,33 @@ public:
 		KING,
 		QUEEN
 	};
+
+	struct Coordinate
+	{
+		Coordinate(std::string coord):
+			x(coord[0]),
+			y(coord[1])
+		{}
+		char x;
+		char y;
+	};
+
 	ChessFigure(FigureType type,std::string coord);
 	virtual bool Move(std::string nextCoord);
 	virtual ~ChessFigure();
+
 protected:
-	bool checkFieldCoordinates(std::string nextCoord);
+
+	bool checkFieldCoordinates(Coordinate coordinate);
+	bool MoveImpl(Coordinate nextCoord);
+
 private:
 	FigureType type;
-	std::string currentCoord;
+	Coordinate currentCoord;
+	const char X_MIN = 'A';
+	const char X_MAX = 'H';
+	const char Y_MIN = '1';
+	const char Y_MAX = '8';
 };
 
 #endif
--- src/ChessFigure.cpp
+++ src/ChessFigure.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 ChessFigure::ChessFigure(ChessFigure::FigureType type, std::string coord) : type(type),
-currentCoord(coord)
+currentCoord(Coordinate(coord))
 {
 }
 
@@ -14,17 +14,22 @@ ChessFigure::~ChessFigure()
 {
 }
 
-bool ChessFigure::checkFieldCoordinates(string nextCoord)
+bool ChessFigure::checkFieldCoordinates(Coordinate coordinate)
 {
-	return nextCoord[0] >= 'A' && nextCoord[0] <= 'H' && nextCoord[1] >= '1' && nextCoord[1] <= '8';
+	return coordinate.x >= X_MIN && coordinate.x <= X_MAX && coordinate.y >= Y_MIN && coordinate.y <= Y_MAX;
 }
 
 bool ChessFigure::Move(string nextCoord)
 {
+	MoveImpl(Coordinate(nextCoord));
+}
+
+bool ChessFigure::MoveImpl(Coordinate nextCoord)
+{
 	if (checkFieldCoordinates(nextCoord))
 	{
-		int deviation_x = abs(nextCoord[0] - currentCoord[0]);
-		int deviation_y = abs(nextCoord[1] - currentCoord[1]);
+		int deviation_x = abs(nextCoord.x - currentCoord.x);
+		int deviation_y = abs(nextCoord.y - currentCoord.y);
 
 		bool x_not_changed = deviation_x == 0;
 		bool y_not_changed = deviation_y == 0;
@@ -32,7 +37,7 @@ bool ChessFigure::Move(string nextCoord)
 		switch (type)
 		{
 		case (PAWN):			
-			if (x_not_changed && (deviation_y >= 1 || (currentCoord[1] == '2' && nextCoord[1] == '4')))
+			if (x_not_changed && (deviation_y >= 1 || (currentCoord.y == '2' && nextCoord.y == '4')))
 			{
 				return true;
 			}
