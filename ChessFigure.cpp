4ac3f5755a8eda4eeb111ee43a0b82bbbcb17ed4
LinuxUser404 <nikolay.pershyn@gmail.com>
2020-09-15 14:59:03 +0300

    Refactor: return default value in the end instead of returning it in every if

--- ChessFigure.cpp
+++ ChessFigure.cpp
@@ -20,24 +20,20 @@ bool Pawn::Move(string nextCoord)
 {
 	if (isWithinBoard(nextCoord))
 	{
-		if (nextCoord[0] != currentCoord[0] || nextCoord[1] <= currentCoord[1] || (nextCoord[1] - currentCoord[1] != 1 && (currentCoord[1] != '2' || nextCoord[1] != '4')))
-			return false;
-		else
+		if (nextCoord[0] == currentCoord[0] && nextCoord[1] > currentCoord[1] && (nextCoord[1] - currentCoord[1] == 1 || (currentCoord[1] == '2' && nextCoord[1] == '4')))
 			return true;
 	}
-	else return false;
+	return false;
 }
 
 bool Rook::Move(string nextCoord)
 {
 	if (isWithinBoard(nextCoord))
 	{
-		if (((nextCoord[0] != currentCoord[0]) && (nextCoord[1] != currentCoord[1])) || ((nextCoord[0] == currentCoord[0]) && (nextCoord[1] == currentCoord[1])))
-			return false;
-		else
+		if (((nextCoord[0] == currentCoord[0]) || (nextCoord[1] == currentCoord[1])) && ((nextCoord[0] != currentCoord[0]) || (nextCoord[1] != currentCoord[1])))
 			return true;
 	}
-	else return false;
+	return false;
 }
 
 bool Knight::Move(string nextCoord)
@@ -47,46 +43,38 @@ bool Knight::Move(string nextCoord)
 		int dx, dy;
 		dx = abs(nextCoord[0] - currentCoord[0]);
 		dy = abs(nextCoord[1] - currentCoord[1]);
-	    if ((abs(nextCoord[0] - currentCoord[0]) != 1 || abs(nextCoord[1] - currentCoord[1]) != 2) && (abs(nextCoord[0] - currentCoord[0]) != 2 || abs(nextCoord[1] - currentCoord[1]) != 1))
-		  return false;
-		else
-		return true;
+	    if ((abs(nextCoord[0] - currentCoord[0]) == 1 && abs(nextCoord[1] - currentCoord[1]) == 2) || (abs(nextCoord[0] - currentCoord[0]) == 2 && abs(nextCoord[1] - currentCoord[1]) == 1))
+			return true;
 	}
-	else return false;
+	return false;
 }
 
 bool Bishop::Move(string nextCoord)
 {
 	if (isWithinBoard(nextCoord))
 	{
-		if (!(abs(nextCoord[0] - currentCoord[0]) == abs(nextCoord[1] - currentCoord[1])))
-			return false;
-		else
+		if (abs(nextCoord[0] - currentCoord[0]) == abs(nextCoord[1] - currentCoord[1]))
 			return true;
 	}
-	else return false;
+	return false;
 }
 
 bool King::Move(string nextCoord)
 {
 	if (isWithinBoard(nextCoord))
 	{
-		if (!(abs(nextCoord[0] - currentCoord[0]) <= 1 && abs(nextCoord[1] - currentCoord[1]) <= 1))
-			return false;
-		else
+		if (abs(nextCoord[0] - currentCoord[0]) <= 1 && abs(nextCoord[1] - currentCoord[1]) <= 1)
 			return true;
 	}
-	else return false;
+	return false;
 }
 
 bool Queen::Move(string nextCoord)
 {
 	if (isWithinBoard(nextCoord))
 	{
-		if (!(abs(nextCoord[0] - currentCoord[0]) == abs(nextCoord[1] - currentCoord[1]) || nextCoord[0] == currentCoord[0] || nextCoord[1] == currentCoord[1]))
-			return false;
-		else
+		if (abs(nextCoord[0] - currentCoord[0]) == abs(nextCoord[1] - currentCoord[1]) || nextCoord[0] == currentCoord[0] || nextCoord[1] == currentCoord[1])
 			return true;
 	}
-	else return false;
+	return false;
 }
