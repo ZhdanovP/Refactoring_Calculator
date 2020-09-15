2e8b635d37f0a2c4ae0a769d77d5b2f0a1649ab1
LinuxUser404 <nikolay.pershyn@gmail.com>
2020-09-15 17:05:24 +0300

    Refactor: move common move checks to the base class

--- ChessFigure.cpp
+++ ChessFigure.cpp
@@ -18,83 +18,62 @@ bool ChessFigure::checkCoords(const std::string& nextCoord) const
   return validColomn && validRow && moves;
 }
 
-
-bool Pawn::Move(const string& nextCoord) const
+bool ChessFigure::Move(const string& nextCoord) const
 {
-  if (checkCoords(nextCoord))
-  {
-    const bool vertical = nextCoord[0] == currentCoord[0];
-    const bool forward = nextCoord[1] > currentCoord[1];
-    const bool firstMove = currentCoord[1] == '2' && nextCoord[1] == '4';
-    const bool oneForward = nextCoord[1] - currentCoord[1] == 1;
-    if (vertical && forward && (oneForward || firstMove))
-      return true;
-  }
+  const int dx = nextCoord[0] - currentCoord[0];
+  const int dy = nextCoord[1] - currentCoord[1];
+  return checkCoords(nextCoord) && peiceSpecificMove(dx, dy);
+}
+
+bool Pawn::peiceSpecificMove(const int dx, const int dy) const
+{
+  const bool vertical = 0 == dx;
+  const bool firstMove2Forward = currentCoord[1] == '2' && dy == 2;
+  const bool oneForward = dy == 1;
+  if (vertical && (oneForward || firstMove2Forward))
+    return true;
   return false;
 }
 
-bool Rook::Move(const string& nextCoord) const
+bool Rook::peiceSpecificMove(const int dx, const int dy) const
 {
-  if (checkCoords(nextCoord))
-  {
-    const bool vertical = nextCoord[0] == currentCoord[0];
-    const bool horizontal = nextCoord[1] == currentCoord[1];
-    if (vertical || horizontal)
-      return true;
-  }
+  const bool vertical = 0 == dx;
+  const bool horizontal = 0 == dy;
+  if (vertical || horizontal)
+    return true;
   return false;
 }
 
-bool Knight::Move(const string& nextCoord) const
+bool Knight::peiceSpecificMove(const int dx, const int dy) const
 {
-  if (checkCoords(nextCoord))
-  {
-    const int dx = nextCoord[0] - currentCoord[0];
-    const int dy = nextCoord[1] - currentCoord[1];
-    const bool g1 = abs(dx) == 1 && abs(dy) == 2;
-    const bool g2 = abs(dx) == 2 && abs(dy) == 1;
-    if (g1 || g2)
-      return true;
-  }
+  const bool g1 = abs(dx) == 1 && abs(dy) == 2;
+  const bool g2 = abs(dx) == 2 && abs(dy) == 1;
+  if (g1 || g2)
+    return true;
   return false;
 }
 
-bool Bishop::Move(const string& nextCoord) const
+bool Bishop::peiceSpecificMove(const int dx, const int dy) const
 {
-  if (checkCoords(nextCoord))
-  {
-    const int dx = nextCoord[0] - currentCoord[0];
-    const int dy = nextCoord[1] - currentCoord[1];
-    const bool diagonal = abs(dx) == abs(dy);
-    if (diagonal)
-      return true;
-  }
+  const bool diagonal = abs(dx) == abs(dy);
+  if (diagonal)
+    return true;
   return false;
 }
 
-bool King::Move(const string& nextCoord) const
+bool King::peiceSpecificMove(const int dx, const int dy) const
 {
-  if (checkCoords(nextCoord))
-  {
-    const int dx = nextCoord[0] - currentCoord[0];
-    const int dy = nextCoord[1] - currentCoord[1];
-    if (abs(dx) <= 1 && abs(dy) <= 1)
-      return true;
-  }
+  if (abs(dx) <= 1 && abs(dy) <= 1)
+    return true;
   return false;
 }
 
-bool Queen::Move(const string& nextCoord) const
+bool Queen::peiceSpecificMove(const int dx, const int dy) const
 {
-  if (checkCoords(nextCoord))
-  {
-    const int dx = nextCoord[0] - currentCoord[0];
-    const int dy = nextCoord[1] - currentCoord[1];
-    const bool vertical = nextCoord[0] == currentCoord[0];
-    const bool horizontal = nextCoord[1] == currentCoord[1];
-    const bool diagonal = abs(dx) == abs(dy);
-    if (diagonal || vertical || horizontal)
-      return true;
-  }
+  const bool vertical = 0 == dx;
+  const bool horizontal = 0 == dy;
+  const bool diagonal = abs(dx) == abs(dy);
+  if (diagonal || vertical || horizontal)
+    return true;
   return false;
 }
--- ChessFigure.hpp
+++ ChessFigure.hpp
@@ -6,72 +6,60 @@ class ChessFigure
 {
 public:
   ChessFigure(std::string coord);
-  virtual bool Move(const std::string& nextCoord) const = 0;
+  bool Move(const std::string& nextCoord) const;
   virtual ~ChessFigure();
 protected:
   bool checkCoords(const std::string& nextCoord) const;
+  virtual bool peiceSpecificMove(const int dx, const int dy) const = 0;
   std::string currentCoord;
 };
 
 class Pawn : public ChessFigure
 {
-  
 public:
   Pawn(std::string coord) : ChessFigure(coord) {}
-  bool Move(const std::string& nextCoord) const override;
-  
-private:
+protected:
+  bool peiceSpecificMove(const int dx, const int dy) const override;
 };
 
 class Rook : public ChessFigure
 {
-  
 public:
   Rook(std::string coord) : ChessFigure(coord) {}
-  bool Move(const std::string& nextCoord) const override;
-  
-private:
+protected:
+  bool peiceSpecificMove(const int dx, const int dy) const override;
 };
 
 class Knight : public ChessFigure
 {
-  
 public:
   Knight(std::string coord) : ChessFigure(coord) {}
-  bool Move(const std::string& nextCoord) const override;
-  
-private:
+protected:
+  bool peiceSpecificMove(const int dx, const int dy) const override;
 };
 
 class Bishop : public ChessFigure
 {
-  
 public:
   Bishop(std::string coord) : ChessFigure(coord) {}
-  bool Move(const std::string& nextCoord) const override;
-  
-private:
+protected:
+  bool peiceSpecificMove(const int dx, const int dy) const override;
 };
 
 class King : public ChessFigure
 {
-  
 public:
   King(std::string coord) : ChessFigure(coord) {}
-  bool Move(const std::string& nextCoord) const override;
-  
-private:
+protected:
+  bool peiceSpecificMove(const int dx, const int dy) const override;
 };
 
 class Queen : public ChessFigure
 {
-  
 public:
   Queen(std::string coord) : ChessFigure(coord) {}
-  bool Move(const std::string& nextCoord) const override;
-  
-private:
+protected:
+  bool peiceSpecificMove(const int dx, const int dy) const override;
 };
-
 
 #endif
