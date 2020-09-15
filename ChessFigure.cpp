0da4e9a1cc8b7aa3248a82bd9334c5bc2fa93626
LinuxUser404 <nikolay.pershyn@gmail.com>
2020-09-15 17:12:42 +0300

    Refactor: remove redundant ifs

--- ChessFigure.cpp
+++ ChessFigure.cpp
@@ -30,42 +30,32 @@ bool Pawn::peiceSpecificMove(const int dx, const int dy) const
   const bool vertical = 0 == dx;
   const bool firstMove2Forward = currentCoord[1] == '2' && dy == 2;
   const bool oneForward = dy == 1;
-  if (vertical && (oneForward || firstMove2Forward))
-    return true;
-  return false;
+  return vertical && (oneForward || firstMove2Forward);
 }
 
 bool Rook::peiceSpecificMove(const int dx, const int dy) const
 {
   const bool vertical = 0 == dx;
   const bool horizontal = 0 == dy;
-  if (vertical || horizontal)
-    return true;
-  return false;
+  return vertical || horizontal;
 }
 
 bool Knight::peiceSpecificMove(const int dx, const int dy) const
 {
   const bool g1 = abs(dx) == 1 && abs(dy) == 2;
   const bool g2 = abs(dx) == 2 && abs(dy) == 1;
-  if (g1 || g2)
-    return true;
-  return false;
+  return g1 || g2;
 }
 
 bool Bishop::peiceSpecificMove(const int dx, const int dy) const
 {
   const bool diagonal = abs(dx) == abs(dy);
-  if (diagonal)
-    return true;
-  return false;
+  return diagonal;
 }
 
 bool King::peiceSpecificMove(const int dx, const int dy) const
 {
-  if (abs(dx) <= 1 && abs(dy) <= 1)
-    return true;
-  return false;
+  return abs(dx) <= 1 && abs(dy) <= 1;
 }
 
 bool Queen::peiceSpecificMove(const int dx, const int dy) const
@@ -73,7 +63,5 @@ bool Queen::peiceSpecificMove(const int dx, const int dy) const
   const bool vertical = 0 == dx;
   const bool horizontal = 0 == dy;
   const bool diagonal = abs(dx) == abs(dy);
-  if (diagonal || vertical || horizontal)
-    return true;
-  return false;
+  return diagonal || vertical || horizontal;
 }
