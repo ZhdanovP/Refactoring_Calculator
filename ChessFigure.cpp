4dc8addf7d80a88d1648a48ab138dda11ff7f387
LinuxUser404 <nikolay.pershyn@gmail.com>
2020-09-15 16:38:06 +0300

    Refactor: fix format and allign

--- ChessFigure.cpp
+++ ChessFigure.cpp
@@ -12,89 +12,89 @@ ChessFigure::~ChessFigure() = default;
 
 bool ChessFigure::checkCoords(const std::string& nextCoord) const
 {
-    const bool validColomn = nextCoord[0] >= 'A' && nextCoord[0] <= 'H';
-    const bool validRow = nextCoord[1] >= '1' && nextCoord[1] <= '8';
-    const bool moves = nextCoord != currentCoord;
-	return validColomn && validRow && moves;
+  const bool validColomn = nextCoord[0] >= 'A' && nextCoord[0] <= 'H';
+  const bool validRow = nextCoord[1] >= '1' && nextCoord[1] <= '8';
+  const bool moves = nextCoord != currentCoord;
+  return validColomn && validRow && moves;
 }
 
 
 bool Pawn::Move(const string& nextCoord) const
 {
-	if (checkCoords(nextCoord))
-	{
-		const bool vertical = nextCoord[0] == currentCoord[0];
-		const bool forward = nextCoord[1] > currentCoord[1];
-		const bool firstMove = currentCoord[1] == '2' && nextCoord[1] == '4';
-		const bool oneForward = nextCoord[1] - currentCoord[1] == 1;
-		if (vertical && forward && (oneForward || firstMove))
-			return true;
-	}
-	return false;
+  if (checkCoords(nextCoord))
+  {
+    const bool vertical = nextCoord[0] == currentCoord[0];
+    const bool forward = nextCoord[1] > currentCoord[1];
+    const bool firstMove = currentCoord[1] == '2' && nextCoord[1] == '4';
+    const bool oneForward = nextCoord[1] - currentCoord[1] == 1;
+    if (vertical && forward && (oneForward || firstMove))
+      return true;
+  }
+  return false;
 }
 
 bool Rook::Move(const string& nextCoord) const
 {
-	if (checkCoords(nextCoord))
-	{
-		const bool vertical = nextCoord[0] == currentCoord[0];
-        const bool horizontal = nextCoord[1] == currentCoord[1];
-		if (vertical || horizontal)
-			return true;
-	}
-	return false;
+  if (checkCoords(nextCoord))
+  {
+    const bool vertical = nextCoord[0] == currentCoord[0];
+    const bool horizontal = nextCoord[1] == currentCoord[1];
+    if (vertical || horizontal)
+      return true;
+  }
+  return false;
 }
 
 bool Knight::Move(const string& nextCoord) const
 {
-	if (checkCoords(nextCoord))
-	{
-		const int dx = nextCoord[0] - currentCoord[0];
-		const int dy = nextCoord[1] - currentCoord[1];
-        const bool g1 = abs(dx) == 1 && abs(dy) == 2;
-        const bool g2 = abs(dx) == 2 && abs(dy) == 1;
-		if (g1 || g2)
-			return true;
-	}
-	return false;
+  if (checkCoords(nextCoord))
+  {
+    const int dx = nextCoord[0] - currentCoord[0];
+    const int dy = nextCoord[1] - currentCoord[1];
+    const bool g1 = abs(dx) == 1 && abs(dy) == 2;
+    const bool g2 = abs(dx) == 2 && abs(dy) == 1;
+    if (g1 || g2)
+      return true;
+  }
+  return false;
 }
 
 bool Bishop::Move(const string& nextCoord) const
 {
-	if (checkCoords(nextCoord))
-	{
-		const int dx = nextCoord[0] - currentCoord[0];
-		const int dy = nextCoord[1] - currentCoord[1];
-		const bool diagonal = abs(dx) == abs(dy);
-		if (diagonal)
-			return true;
-	}
-	return false;
+  if (checkCoords(nextCoord))
+  {
+    const int dx = nextCoord[0] - currentCoord[0];
+    const int dy = nextCoord[1] - currentCoord[1];
+    const bool diagonal = abs(dx) == abs(dy);
+    if (diagonal)
+      return true;
+  }
+  return false;
 }
 
 bool King::Move(const string& nextCoord) const
 {
-	if (checkCoords(nextCoord))
-	{
-		const int dx = nextCoord[0] - currentCoord[0];
-		const int dy = nextCoord[1] - currentCoord[1];
-		if (abs(dx) <= 1 && abs(dy) <= 1)
-			return true;
-	}
-	return false;
+  if (checkCoords(nextCoord))
+  {
+    const int dx = nextCoord[0] - currentCoord[0];
+    const int dy = nextCoord[1] - currentCoord[1];
+    if (abs(dx) <= 1 && abs(dy) <= 1)
+      return true;
+  }
+  return false;
 }
 
 bool Queen::Move(const string& nextCoord) const
 {
-	if (checkCoords(nextCoord))
-	{
-		const int dx = nextCoord[0] - currentCoord[0];
-		const int dy = nextCoord[1] - currentCoord[1];
-		const bool vertical = nextCoord[0] == currentCoord[0];
-		const bool horizontal = nextCoord[1] == currentCoord[1];
-		const bool diagonal = abs(dx) == abs(dy);
-		if (diagonal || vertical || horizontal)
-			return true;
-	}
-	return false;
+  if (checkCoords(nextCoord))
+  {
+    const int dx = nextCoord[0] - currentCoord[0];
+    const int dy = nextCoord[1] - currentCoord[1];
+    const bool vertical = nextCoord[0] == currentCoord[0];
+    const bool horizontal = nextCoord[1] == currentCoord[1];
+    const bool diagonal = abs(dx) == abs(dy);
+    if (diagonal || vertical || horizontal)
+      return true;
+  }
+  return false;
 }
--- ChessFigure.hpp
+++ ChessFigure.hpp
@@ -5,71 +5,71 @@
 class ChessFigure
 {
 public:
-	ChessFigure(std::string coord);
-	virtual bool Move(const std::string& nextCoord) const = 0;
-	virtual ~ChessFigure();
+  ChessFigure(std::string coord);
+  virtual bool Move(const std::string& nextCoord) const = 0;
+  virtual ~ChessFigure();
 protected:
-	bool checkCoords(const std::string& nextCoord) const;
-	std::string currentCoord;
+  bool checkCoords(const std::string& nextCoord) const;
+  std::string currentCoord;
 };
 
 class Pawn : public ChessFigure
 {
-	
+  
 public:
-	Pawn(std::string coord) : ChessFigure(coord) {}
-	bool Move(const std::string& nextCoord) const override;
-
+  Pawn(std::string coord) : ChessFigure(coord) {}
+  bool Move(const std::string& nextCoord) const override;
+  
 private:
 };
 
 class Rook : public ChessFigure
 {
-	
+  
 public:
-	Rook(std::string coord) : ChessFigure(coord) {}
-	bool Move(const std::string& nextCoord) const override;
-
+  Rook(std::string coord) : ChessFigure(coord) {}
+  bool Move(const std::string& nextCoord) const override;
+  
 private:
 };
 
 class Knight : public ChessFigure
 {
-	
+  
 public:
-	Knight(std::string coord) : ChessFigure(coord) {}
-	bool Move(const std::string& nextCoord) const override;
-
+  Knight(std::string coord) : ChessFigure(coord) {}
+  bool Move(const std::string& nextCoord) const override;
+  
 private:
 };
 
 class Bishop : public ChessFigure
 {
-	
+  
 public:
-	Bishop(std::string coord) : ChessFigure(coord) {}
-	bool Move(const std::string& nextCoord) const override;
-
+  Bishop(std::string coord) : ChessFigure(coord) {}
+  bool Move(const std::string& nextCoord) const override;
+  
 private:
 };
 
 class King : public ChessFigure
 {
-	
+  
 public:
-	King(std::string coord) : ChessFigure(coord) {}
-	bool Move(const std::string& nextCoord) const override;
-
+  King(std::string coord) : ChessFigure(coord) {}
+  bool Move(const std::string& nextCoord) const override;
+  
 private:
 };
 
 class Queen : public ChessFigure
 {
-	
+  
 public:
-	Queen(std::string coord) : ChessFigure(coord) {}
-	bool Move(const std::string& nextCoord) const override;
-
+  Queen(std::string coord) : ChessFigure(coord) {}
+  bool Move(const std::string& nextCoord) const override;
+  
 private:
 };
 
